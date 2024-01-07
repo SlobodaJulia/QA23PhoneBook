c8088c5b106b9ff01897cab9ee872485cb8a0341
SlobodaJulia <slobodajulia29@gmail.com>
2024-01-07 13:39:17 +0200

    NegativeLogin

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
 
 import java.util.List;
 import java.util.Set;
@@ -12,12 +13,19 @@ public class ApplicationManager {
 
 
     WebDriver wd;
+    HelperUser helperUser;
 
     public void init(){
         wd = new ChromeDriver();
-        wd.navigate().to("https://telranedu.web.app/home");
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.navigate().to("https://telranedu.web.app/");
+        helperUser = new HelperUser(wd);
     }
 
+    public HelperUser getHelperUser() {
+        return helperUser;
+    }
 
     public void stop(){
         wd.quit();
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,60 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
-    @Test
-    public void Test(){
-        
+
+    @BeforeMethod
+    public void preCondition(){
+        //if SingOut present --->logout
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
     }
+
+    @Test
+    public void loginSuccess(){
+        app.getHelperUser().openLoginRegistForm();
+        app.getHelperUser().fillLoginRegistrationForm("alexlev@google.com", "220821SBsb)(!");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+
+    }
+    @Test
+    public void loginSuccessModel() {
+        app.getHelperUser().openLoginRegistForm();
+        app.getHelperUser().fillLoginRegistrationForm("alexlev@google.com", "220821SBsb)(!");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+    }
+    @Test
+    public void loginWrongEmail(){
+        app.getHelperUser().openLoginRegistForm();
+        app.getHelperUser().fillLoginRegistrationForm("alexlevgoogle.com", "220821SBsb)(!");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password"));
+    }
+    @Test
+    public void loginWrongPassword(){
+        app.getHelperUser().openLoginRegistForm();
+        app.getHelperUser().fillLoginRegistrationForm("alexlev@google.com", "ndjdj");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password"));
+
+
+    }
+    @Test
+    public void loginUnregisteredUser(){
+        app.getHelperUser().openLoginRegistForm();
+        app.getHelperUser().fillLoginRegistrationForm("luck@gmail.com", "Lluk123456$");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password"));
+
+    }
+
 
 }
